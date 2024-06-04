9f00f0523b961154ef8b7959dbea1d96be17901a
Nathan Dedman <nathan.r.dedman@gmail.com>
2024-06-04 20:36:08 +0000

    Docstrings Remove deprecated function

--- src/task_api/auth/utils.py
+++ src/task_api/auth/utils.py
@@ -3,14 +3,28 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
 def hash_password(password: str) -> str:
+    """Hash a password using the Bcrypt
+    hashpw function.
+
+    Args:
+        password (str): Password string
+
+    Returns:
+        str: Computed Hash
+    """
     return bcrypt.hashpw(password.encode("utf8"), bcrypt.gensalt()).decode("utf8")
 
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password, hashed_password) -> bool:
+    """Verify a password against the hashed version
+    using bcrypt checkpw function.
+
+    Args:
+        plain_password (str): Password
+        hashed_password (bool): Prehashed password (from DB)
+
+    Returns:
+        bool: Verification of password
+    """
     return bcrypt.checkpw(plain_password.encode(), hashed_password.encode())
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
