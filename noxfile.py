cb170a1860565bb550ce4d974aba55fc415b5783
Nathan Dedman <nathan.r.dedman@gmail.com>
2024-06-05 10:59:29 +0000

    Slight refactor Update noxfile Update mypy config

--- noxfile.py
+++ noxfile.py
@@ -6,30 +6,30 @@ PY_VERSIONS = ["3.9", "3.10"]
 @nox.session(python=PY_VERSIONS)
 def test(session):
     session.install("pytest")
-    session.run("pytest", "test/")
+    session.run("pytest", "tests/")
 
 
 @nox.session(python=PY_VERSIONS)
 def formatcheck(session):
     session.install("black")
-    session.run("black", "--check", "src/", "test/")
+    session.run("black", "--check", "src/", "tests/")
 
 
 @nox.session(python=PY_VERSIONS)
-def typecheck(session):
-    session.install("mypy", "pytest", "sqlalchemy", "sqlalchemy_utils")
-    session.run("pip", "install", "-r", "mypy_requirements.txt")
-    session.run("mypy", "--install-types", "--non-interactive")
-    session.run("mypy", "--no-namespace-packages", "src/", "test/")
+def typecheck(session):    
+    session.install("mypy")
+    # session.run("pip", "install", "-r", "mypy_requirements.txt")
+    # session.run("mypy", "--install-types", "--non-interactive")
+    session.run("mypy", "--no-namespace-packages", "src/", "tests/")
 
 
 @nox.session(python=PY_VERSIONS)
 def lint(session):
     session.install("pylint")
-    session.run("pylint", "src/", "test/")
+    session.run("pylint", "src/", "tests/")
 
 
 @nox.session(python=PY_VERSIONS)
 def importscheck(session):
     session.install("isort")
-    session.run("isort", "--check", "src/", "test/")
+    session.run("isort", "--check", "src/", "tests/")
